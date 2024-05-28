e34e159c19824d48eaf70f986307aaf385ac42bf
Professorelieser <elieser.dos.santos@escola.pr.gov.br>
2024-05-28 11:17:20 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -14,14 +14,11 @@ for (let i = 0; i < botoes.length; i++) {
     }
 }
 
-
-}
-
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2020-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+const tempoObjetivo1 = new Date("2024-12-31T00:00:00");
+const tempoObjetivo2 = new Date("2024-12-21T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-11T00:00:00");
+const tempoObjetivo4 = new Date("2024-12-01T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
@@ -45,26 +42,14 @@ function calculaTempo(tempoObjetivo) {
 }
 
 function atualizaCronometro(){
-    for(let i=0; i<contadores.length; i++){
-        contadores[i].textContent=calculaTempo(tempos[1]);
+    for (let i=0; i<contadores.length;i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);   
     }
 }
 
 function comecaCronometro(){
     atualizaCronometro();
-    setInterval(atualizaCronometro, 1000);
+    setInterval(atualizaCronometro,1000);
 }
 
 comecaCronometro();
-
-
-
-
-
-
-
-
-
-
-
-
